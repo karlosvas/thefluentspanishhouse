88faf2af76db124c017f7218e28ed0698105eb5a
karlosvas <126987511+karlosvas@users.noreply.github.com>
2024-08-21 10:15:17 +0200

    Update load banner

--- client/src/components/pages-components/main/PlaceholderBanner.tsx
+++ client/src/components/pages-components/main/PlaceholderBanner.tsx
@@ -1,15 +1,5 @@
 const PlaceholderBanner = () => {
-  return (
-    <>
-      {window.innerWidth >= 1080 ? (
-        <div className="card-img-placeholder" style={{ height: "55vh" }}></div>
-      ) : window.innerWidth >= 766 ? (
-        <div className="card-img-placeholder" style={{ height: "55vh" }}></div>
-      ) : (
-        <div className="card-img-placeholder" style={{ height: "50vh" }}></div>
-      )}
-    </>
-  );
+  return <div className="card-img-placeholder" id="banner"></div>;
 };
 
 export default PlaceholderBanner;
--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,42 +1,17 @@
 import Prices from "../components/pages-components/main/Prices";
 import PlaceholderBanner from "../components/pages-components/main/PlaceholderBanner";
-import { useEffect, useState, useCallback } from "react";
+import { useState } from "react";
 import "../styles/main.css";
 
 const Main = () => {
   const [imageLoaded, setImageLoaded] = useState<boolean>(false);
-  const [imageUrl, setImageUrl] = useState<string>("");
-
-  // Función para obtener la URL de la imagen según el tamaño de la ventana
-  const getImageUrl = useCallback(() => {
-    if (window.innerWidth >= 1024) return "/img/banner-xl.webp";
-    if (window.innerWidth >= 766) return "/img/banner-md.webp";
-    return "/img/banner-sm.webp";
-  }, []);
-
-  useEffect(() => {
-    const handleResize = () => {
-      const url = getImageUrl();
-      // Pre-cargar la imagen
-      const img = new Image();
-      img.src = url;
-      img.onload = () => {
-        setImageUrl(url);
-        setImageLoaded(true);
-      };
-    };
-
-    handleResize(); // Establece la URL inicial de la imagen
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [getImageUrl]);
 
   return (
     <>
       {!imageLoaded && <PlaceholderBanner />}
       <img
         id="banner"
-        src={imageUrl}
+        src="/img/banner-xl.webp"
         alt="Banner with inspiring images"
         onError={() => setImageLoaded(false)}
         style={{
